318989b63652a88cba5fa4619bd6cac5035c19dd
l1h3r <5410284+l1h3r@users.noreply.github.com>
2025-02-22 11:25:21 +0400

    Implement SpecShift for std integer sizes

--- crates/exint-backend/src/export/shift.rs
+++ crates/exint-backend/src/export/shift.rs
@@ -1,3 +1,5 @@
+use crate::macros::specialize;
+use crate::traits::Cast;
 use crate::types::Int;
 
 // -----------------------------------------------------------------------------
@@ -33,6 +35,37 @@ impl<const S: usize> const SpecShift for Int<S> {
 // -----------------------------------------------------------------------------
 // Implementation - Specialization for 'std' sizes
 // -----------------------------------------------------------------------------
+
+specialize! {
+  impl SpecShift for Int<1|2|4|8|16> {
+    // LLVM generates `shl $type` instruction
+    #[inline]
+    unsafe fn shl(self, bits: u32) -> Self {
+      // SAFETY: This is guaranteed to be safe by the caller.
+      unsafe {
+        ::core::intrinsics::unchecked_shl(self.ucast(), bits).ucast()
+      }
+    }
+
+    // LLVM generates `lshr $type` instruction
+    #[inline]
+    unsafe fn lshr(self, bits: u32) -> Self {
+      // SAFETY: This is guaranteed to be safe by the caller.
+      unsafe {
+        ::core::intrinsics::unchecked_shr(self.ucast(), bits).ucast()
+      }
+    }
+
+    // LLVM generates `ashr $type` instruction
+    #[inline]
+    unsafe fn ashr(self, bits: u32) -> Self {
+      // SAFETY: This is guaranteed to be safe by the caller.
+      unsafe {
+        ::core::intrinsics::unchecked_shr(self.scast(), bits).scast()
+      }
+    }
+  }
+}
 
 // -----------------------------------------------------------------------------
 // Implementation - Specialization for common sizes
