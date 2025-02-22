9300151a67be9ad7e74538c3b07380e6528505e2
l1h3r <5410284+l1h3r@users.noreply.github.com>
2025-02-22 11:25:22 +0400

    Implement unbounded_{shl|shr}

--- crates/exint-integer/src/macros/arithmetic/unbounded.rs
+++ crates/exint-integer/src/macros/arithmetic/unbounded.rs
@@ -2,21 +2,49 @@ macro_rules! unbounded {
   (core, $name:ident, $uint:expr) => {
     #[cfg(feature = "unbounded_shifts")]
     #[doc(cfg(feature = "unbounded_shifts"))]
+    #[must_use]
+    #[inline]
     pub const fn unbounded_shl(self, rhs: u32) -> Self {
-      panic!("unbounded_shl")
-    }
-
-    #[cfg(feature = "unbounded_shifts")]
-    #[doc(cfg(feature = "unbounded_shifts"))]
-    pub const fn unbounded_shr(self, rhs: u32) -> Self {
-      panic!("unbounded_shr")
+      if rhs < Self::BITS {
+        // SAFETY: We just ensured that `rhs` is in-range.
+        unsafe { self.unchecked_shl(rhs) }
+      } else {
+        Self::ZERO
+      }
     }
   };
   (uint) => {
     $crate::macros::unbounded!(core, uint, true);
+
+    #[cfg(feature = "unbounded_shifts")]
+    #[doc(cfg(feature = "unbounded_shifts"))]
+    #[must_use]
+    #[inline]
+    pub const fn unbounded_shr(self, rhs: u32) -> Self {
+      if rhs < Self::BITS {
+        // SAFETY: We just ensured that `rhs` is in-range.
+        unsafe { self.unchecked_shr(rhs) }
+      } else {
+        Self::ZERO
+      }
+    }
   };
   (int) => {
     $crate::macros::unbounded!(core, int, false);
+
+    #[cfg(feature = "unbounded_shifts")]
+    #[doc(cfg(feature = "unbounded_shifts"))]
+    #[must_use]
+    #[inline]
+    pub const fn unbounded_shr(self, rhs: u32) -> Self {
+      if rhs < Self::BITS {
+        // SAFETY: We just ensured that `rhs` is in-range.
+        unsafe { self.unchecked_shr(rhs) }
+      } else {
+        // SAFETY: `Self::BITS - 1` is guaranteed to be less than `Self::BITS`.
+        unsafe { self.unchecked_shr(Self::BITS - 1) }
+      }
+    }
   };
 }
 
