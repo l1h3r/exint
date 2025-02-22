4439b2c8d7454791289cbe77943a2dc438e6684a
l1h3r <5410284+l1h3r@users.noreply.github.com>
2025-02-22 11:25:22 +0400

    Add feature-gated unstable marker traits
    - StructuralPartialEq
    - UnsizedConstParamTy
    - ConstParamTy

--- src/types/traits/marker.rs
+++ src/types/traits/marker.rs
@@ -1,6 +1,15 @@
 macro_rules! implement {
   ($name:ident) => {
     impl<const N: usize> ::core::marker::Copy for $crate::$name<N> {}
+
+    #[cfg(feature = "structural_match")]
+    impl<const N: usize> ::core::marker::StructuralPartialEq for $crate::$name<N> {}
+
+    #[cfg(feature = "unsized_const_params")]
+    impl<const N: usize> ::core::marker::UnsizedConstParamTy for $crate::$name<N> {}
+
+    #[cfg(feature = "unsized_const_params")]
+    impl<const N: usize> ::core::marker::ConstParamTy_ for $crate::$name<N> {}
   };
 }
 
